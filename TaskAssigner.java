3ef854eef07af32d0a27777e206b60b6b0f51e2a
Amani A <anderson.amani13@gmail.com>
2024-05-06 22:27:36 -0400

    Added more helper methods to task assigner

--- TaskAssigner.java
+++ TaskAssigner.java
@@ -1,12 +1,31 @@
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 public class TaskAssigner {
     private Random rand;
+    private List<Task> tasks;
 
     public TaskAssigner() {
         rand = new Random();
+        tasks = new ArrayList<>();
+    }
+
+    public void addTask(Task task) {
+        tasks.add(task);
+    }
+
+    public void deleteTask(Task task) {
+        tasks.remove(task);
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public Task getRandomTask() {
+        return tasks.get(rand.nextInt(tasks.size()));
     }
 
 }
